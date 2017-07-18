bc66df6256aba41372131d7b79492f96b952541a
Viktor Khon <betepokus@yahoo.com>
2017-07-17 23:22:22 -0400

    Add menu button to EditActivity
    - Download some extra pictures from Pets project

--- app/src/main/java/com/viktorkhon/udacity_project_10_inventoryapp/EditActivity.java
+++ app/src/main/java/com/viktorkhon/udacity_project_10_inventoryapp/EditActivity.java
@@ -2,6 +2,7 @@ package com.viktorkhon.udacity_project_10_inventoryapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 
 /**
  * Created by Viktor Khon on 7/17/2017.
@@ -12,5 +13,13 @@ public class EditActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu options from the res/menu/menu_editor.xml file.
+        // This adds menu items to the app bar.
+        getMenuInflater().inflate(R.menu.menu_edit, menu);
+        return true;
     }
 }
