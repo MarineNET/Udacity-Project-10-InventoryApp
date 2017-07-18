0cab47c238def3fa99340515bf2253b1966cdfd9
Viktor Khon <betepokus@yahoo.com>
2017-07-18 15:43:15 -0400

    Make a TextView start an intent that lets a user to choose a picture from phone

--- app/src/main/java/com/viktorkhon/udacity_project_10_inventoryapp/EditActivity.java
+++ app/src/main/java/com/viktorkhon/udacity_project_10_inventoryapp/EditActivity.java
@@ -1,18 +1,50 @@
 package com.viktorkhon.udacity_project_10_inventoryapp;
 
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
+
+import com.viktorkhon.udacity_project_10_inventoryapp.Data.InventoryContract.InventoryEntry;
+
+import com.viktorkhon.udacity_project_10_inventoryapp.Data.InventoryContract;
 
 /**
  * Created by Viktor Khon on 7/17/2017.
  */
 
 public class EditActivity extends AppCompatActivity{
+
+    private static final int READ_REQUEST_CODE = 42;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor);
+
+        TextView addImage = (TextView) findViewById(R.id.add_image);
+        addImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                    // ACTION_OPEN_DOCUMENT is the intent to choose a file via the system's file
+                    // browser.
+                    Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+
+                    // Filter to only show results that can be "opened", such as a
+                    // file (as opposed to a list of contacts or timezones)
+                    intent.addCategory(Intent.CATEGORY_OPENABLE);
+
+                    // Filter to show only images, using the image MIME data type.
+                    intent.setType("image/*");
+
+                    startActivityForResult(intent, READ_REQUEST_CODE);
+            }
+        });
     }
 
     @Override
