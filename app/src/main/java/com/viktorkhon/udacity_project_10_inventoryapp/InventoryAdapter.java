75486173e09bc53607712a70be4cb4b4afda06db
Viktor Khon <betepokus@yahoo.com>
2017-07-22 01:04:13 -0400

    Create sale button in ListView to subtract quantities

--- app/src/main/java/com/viktorkhon/udacity_project_10_inventoryapp/InventoryAdapter.java
+++ app/src/main/java/com/viktorkhon/udacity_project_10_inventoryapp/InventoryAdapter.java
@@ -1,7 +1,10 @@
 package com.viktorkhon.udacity_project_10_inventoryapp;
 
+import android.content.ContentUris;
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -28,34 +31,62 @@ public class InventoryAdapter extends CursorAdapter {
         return LayoutInflater.from(context).inflate(R.layout.lits_item, viewGroup, false);
     }
 
-    int quantity;
-    TextView qtyView;
-
+    /**
+     * Bind an existing view to the data pointed to by cursor
+     * @param view - Individual View in a ListView
+     * @param context - CatalogActivity
+     * @param cursor - The cursor from which to get the data.
+     *               The cursor is already moved to the correct position.
+     */
     @Override
-    public void bindView(View view, Context context, Cursor cursor) {
-        TextView nameView = (TextView)view.findViewById(R.id.lv_name);
-        qtyView = (TextView)view.findViewById(R.id.lv_quantity);
-        TextView priceView = (TextView)view.findViewById(R.id.lv_price);
-        Button saleButton = (Button)view.findViewById(R.id.lv_button);
+    public void bindView(View view, Context context, final Cursor cursor) {
 
+        // Give variables to each of the views in list_items.xml
+        TextView nameView = (TextView) view.findViewById(R.id.lv_name);
+        final TextView qtyView = (TextView) view.findViewById(R.id.lv_quantity);
+        TextView priceView = (TextView) view.findViewById(R.id.lv_price);
+        Button saleButton = (Button) view.findViewById(R.id.lv_button);
+
+        // Take out the value of each column and store it in a variable
         String name = cursor.getString(cursor.getColumnIndexOrThrow(InventoryEntry.COLUMN_NAME));
-        quantity = cursor.getInt(cursor.getColumnIndexOrThrow(InventoryEntry.COLUMN_QTY));
+        int quantity = cursor.getInt(cursor.getColumnIndexOrThrow(InventoryEntry.COLUMN_QTY));
         int price = cursor.getInt(cursor.getColumnIndexOrThrow(InventoryEntry.COLUMN_PRICE));
 
-        // At this time only the last row is being changed, no matter where you press this button
-        saleButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                quantity--;
-                qtyView.setText(String.valueOf(quantity));
-                if(quantity < 0) {
-                    quantity = 5;
-                }
-            }
-        });
-
+        // Assign a value extracted from the table to corresponding View in the ListView
         nameView.setText(name);
         qtyView.setText(String.valueOf(quantity));
         priceView.setText(String.valueOf(price));
+
+        // Gets position of a cursor in the list
+        final int position = cursor.getPosition();
+
+        // Credits to Udacity forum and mentors with figuring out the whole code
+        saleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ContentValues values = new ContentValues();
+
+                Uri uri = null;
+                // Upon click, get cursor of the specified position
+                cursor.moveToPosition(position);
+                // get _id number of _ID column
+                long id = cursor.getLong(cursor.getColumnIndexOrThrow(InventoryEntry.COLUMN_ID));
+
+                // uri gets the id number of a clicked row
+                Uri idToChange = ContentUris.withAppendedId(InventoryEntry.CONTENT_URI, id);
+
+                    // Extract an int value from the view that holds quantities
+                    int quantity = Integer.parseInt(qtyView.getText().toString());
+                    quantity --;
+                    if (quantity <= 0) {
+                        quantity = 0;
+                    }
+
+                // create a new value for the quatity column and add the updated number to the column
+                values.put(InventoryEntry.COLUMN_QTY, quantity);
+                // Update the table with the new quantity value
+                view.getContext().getContentResolver().update(idToChange, values, null, null);
+            }
+        });
     }
 }
